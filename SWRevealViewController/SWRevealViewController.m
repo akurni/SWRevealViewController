24021bc027fc28a6ed487b45340be88e5ec2cd42
irfan <muhammad.irfan@at.co.id>
2014-02-27 17:05:49 +0700

    back to orientation all

--- SWRevealViewController/SWRevealViewController.m
+++ SWRevealViewController/SWRevealViewController.m
@@ -605,7 +605,7 @@ static NSString * const SWSegueRightIdentifier = @"sw_right";
 
 - (NSUInteger)supportedInterfaceOrientations
 {
-    return UIInterfaceOrientationMaskPortrait;
+    return UIInterfaceOrientationMaskAll;
 }
 
 // Support for earlier than iOS 6.0
